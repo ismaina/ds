697ff0c6c1e725d2f8e21a9029f6c5043614c96c
ismaina <maina.wanjau@gmail.com>
2019-01-28 11:39:57 +0300

    Assignment 1 imporved

--- Data Science/assignment1.py
+++ Data Science/assignment1.py
@@ -1,7 +1,28 @@
 import math
 #How to check the log of x to the power of y
-x= int(input("Enter your first number : "))
-y=int(input("Enter your second number: "))
-print("{0} to the power of {1} is ".format(x,y), str(x**y))
-l= math.log(x)
-print("Log of {0} is ".format(x),str(l) )
+
+for p in range(1,10,1):
+    
+    while True:
+        try:
+            x=int(input("Enter your first number : "))
+        except ValueError:
+            print(' Print Please enter a valid number')
+            continue
+        else:
+            break
+
+
+
+    while True:
+        try:
+            y=int(input("Enter your Second number : "))
+        except ValueError:
+            print(' Print Please enter a valid number')
+            continue
+        else:
+            break
+
+    print("{0} raised to the power of {1} is : ".format(x,y),pow(x, y))
+    l= math.log(x)
+    print("Log of {0} is ".format(x),str(l) )
